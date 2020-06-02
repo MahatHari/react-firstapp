564424d3cedfabad83f213924dbcc673930802e6
Hari Mahat <hmahat@gmail.com>
2020-06-02 18:09:55 +0300

    Updates

---
+++ precache-manifest.b188eb242d19df8956686efae65433ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d355b7518d3f8094a7e607b28a72d2",
+    "url": "/react-firstapp/index.html"
+  },
+  {
+    "revision": "2e7e20cc39d28ad62218",
+    "url": "/react-firstapp/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "1f5ef775e0adec5c5b39",
+    "url": "/react-firstapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e7e20cc39d28ad62218",
+    "url": "/react-firstapp/static/js/2.8e95c0c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-firstapp/static/js/2.8e95c0c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5ef775e0adec5c5b39",
+    "url": "/react-firstapp/static/js/main.bea2c32b.chunk.js"
+  },
+  {
+    "revision": "bfd21ccc0269c9381a53",
+    "url": "/react-firstapp/static/js/runtime-main.22c66649.js"
+  }
+]);
